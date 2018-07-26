c69e2ec4616af12b1ac0c69a5998ac0348dabc88
yysfire <yysfire+repo@gmail.com>
2018-07-26 16:24:50 +0800

    solve leap

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_leap_year(year: i32) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    if year % 400 == 0 {
+        true
+    } else if year % 4 == 0 && year % 100 != 0 {
+        true
+    } else {
+        false
+    }
 }
