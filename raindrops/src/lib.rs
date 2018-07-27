623c3d1e1d94b97a1150bc8057f40ac314fea2fd
yysfire <yysfire+repo@gmail.com>
2018-07-27 17:37:42 +0800

    solve raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut result = String::new();
+    if n % 3 != 0 && n % 5 != 0 && n % 7 != 0 {
+        return format!("{}", n);
+    }
+    if n % 3 == 0 {
+        result.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        result.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        result.push_str("Plong");
+    }
+    result
 }
