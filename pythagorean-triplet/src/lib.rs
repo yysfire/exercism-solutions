3f648680211949521bdc00cc115bd2db4cb58222
yysfire <yysfire+repo@gmail.com>
2018-07-27 14:02:35 +0800

    Optimize solution of pythagorean-triplet

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,11 +1,8 @@
-#![allow(unused_variables)]
 pub fn find() -> Option<u32> {
-    let a: u32;
-    let b: u32;
     for a in 1..1000-1 {
         for b in 1..1000-a {
             let c: u32 = 1000 - a - b;
-            if c <= 0 {
+            if c >= a + b {
                 continue;
             } else if a * a + b * b == c * c {
                 println!("a: {}, b: {}, c: {}", a, b, c);
