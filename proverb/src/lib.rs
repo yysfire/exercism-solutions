143de3c7a61ee408db020067de0f0e019f0b0cd1
yysfire <yysfire+repo@gmail.com>
2018-07-30 16:20:21 +0800

    solve proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let len = list.len();
+    if len == 0 {
+        return String::new();
+    }
+
+    let last = format!("And all for the want of a {}.", list[0]);
+    let mut result = String::new();
+    for i in 0..len - 1 {
+        let s = format!("For want of a {} the {} was lost.\n", list[i], list[i+1]);
+        result.push_str(&s);
+    }
+    result.push_str(&last);
+
+    result
 }
