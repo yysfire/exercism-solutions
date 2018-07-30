5c8f6bb925f7887c3727b26f1b0fb52b311ad337
yysfire <yysfire+repo@gmail.com>
2018-07-30 13:09:07 +0800

    solve nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,32 @@
 pub fn nth(n: u32) -> Option<u32> {
-    unimplemented!("What is the {}th prime number?", n)
+    if n == 0 {
+        return None;
+    }
+    let mut count = 0;
+    for num in 2..=u32::max_value() {
+        if is_prime(num) {
+            count += 1;
+            if count == n {
+                return Some(num);
+            }
+        }
+    }
+    None
+}
+
+pub fn is_prime(num: u32) -> bool {
+    if num == 0 || num == 1 {
+        return false;
+    } else if num == 2 {
+        return true;
+    }
+    for i in 2..num {
+        if num % i == 0 {
+            return false;
+        }
+        if i.pow(2) > num {
+            break;
+        }
+    }
+    true
 }
