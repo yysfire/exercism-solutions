92dfe356cce0b854251c8c00f8b78c3f216d8abe
yysfire <yysfire+repo@gmail.com>
2018-07-31 12:03:53 +0800

    solve collatz-conjecture

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,23 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    if n == 0 {
+        return None;
+    }
+
+    let mut n = n;
+    let mut steps = 0;
+    loop {
+        if n == 1 {
+            break;
+        }
+
+        if n % 2 == 0 {
+            n /= 2;
+            steps += 1;
+        } else {
+            n = n * 3 + 1;
+            steps += 1;
+        }
+    }
+
+    Some(steps)
 }
