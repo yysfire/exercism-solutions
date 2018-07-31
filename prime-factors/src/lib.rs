d539e3a1352ce274c27d4091424ee361b1d0afaf
yysfire <yysfire+repo@gmail.com>
2018-07-31 10:20:00 +0800

    solve prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    if n == 0 || n == 1 {
+        return vec![];
+    }
+
+    let mut m = n;
+    let mut result = vec![];
+    let mut d = 2;
+    loop {
+        if m % d != 0 {
+            d += 1;
+            continue;
+        }
+        result.push(d);
+        m /= d;
+        if m == 1 {
+            break;
+        }
+    }
+
+    result
 }
