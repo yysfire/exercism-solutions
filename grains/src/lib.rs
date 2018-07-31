5e42e38eb108774ab7d43363988cc73847d72b79
yysfire <yysfire+repo@gmail.com>
2018-07-31 11:54:52 +0800

    solve grains

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s == 0 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2_u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).fold(0, |acc, x| acc + square(x))
 }
