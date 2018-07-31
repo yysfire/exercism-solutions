b2e8e5e1ec658de85a9cc9829a0895aa941fdc5d
yysfire <yysfire+repo@gmail.com>
2018-07-31 11:40:35 +0800

    solve series

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    let str_len = digits.len();
+    if len == 0 {
+        return vec!["".to_string(); str_len + 1];
+    }
+
+    let mut result = Vec::new();
+    for i in 0..str_len {
+        if i + len <= str_len {
+            result.push(digits[i..i+len].to_string());
+        }
+    }
+
+    result
 }
