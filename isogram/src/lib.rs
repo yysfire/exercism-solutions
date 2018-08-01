65d45ca94cde630c8eba28ab35733e4d35a4f3fb
yysfire <yysfire+repo@gmail.com>
2018-08-01 09:47:16 +0800

    solve isogram

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,16 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut candidate = candidate.to_ascii_lowercase();
+    candidate.retain(|c| c != '-' && c != ' ');
+
+    let mut set = HashSet::new();
+    for c in candidate.chars() {
+        if !c.is_ascii_alphabetic() || set.contains(&c) {
+            return false;
+        }
+        set.insert(c);
+    }
+
+    true
 }
