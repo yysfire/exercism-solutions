f0404d7453379e7286324fd104d3477a9f155b3f
yysfire <yysfire+repo@gmail.com>
2018-08-01 14:31:38 +0800

    solve pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,4 +1,7 @@
+use std::collections::HashSet;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    let set: HashSet<char> = sentence.to_ascii_lowercase().chars().collect();
+    (97u8..123).filter(|x| set.contains(&char::from(*x))).count() == 26
 }
