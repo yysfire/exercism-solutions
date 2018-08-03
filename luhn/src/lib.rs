c9b9cb58a65967849c709c047e64f47a205fa230
yysfire <yysfire+repo@gmail.com>
2018-08-03 10:50:53 +0800

    solve luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,22 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let mut code = code.to_owned();
+    code.retain(|x| x != ' ');
+    if code.len() < 2 {
+        return false;
+    }
+
+    if !code.chars().all(|x| x.is_ascii_digit()) {
+        return false;
+    }
+
+    let sum: u32 = code.chars()
+        .map(|x| x.to_digit(10).unwrap())
+        .rev()
+        .enumerate()
+        .map(|(i, x)|{ if i % 2 == 1 { x * 2 } else { x } })
+        .map(|x|{ if x > 9 { x - 9 } else { x } })
+        .sum();
+
+    sum % 10 == 0
 }
