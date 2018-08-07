c59d373883a9121392917ae459fc72ed991753c8
yysfire <yysfire+repo@gmail.com>
2018-08-07 17:51:13 +0800

    solve atbash-cipher

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,9 +1,44 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    let plain = plain.to_ascii_lowercase();
+    let iter = plain
+        .chars()
+        .filter(|x| x.is_ascii_alphanumeric())
+        .map(|x| {
+            if x.is_ascii_alphabetic() {
+                cipher_char(x)
+            } else {
+                x
+            }
+        });
+    let mut result = String::new();
+    for (i, c) in iter.enumerate() {
+        if i != 0 && i % 5 == 0 {
+            result.push(' ');
+        }
+        result.push(c);
+    }
+
+    result
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    cipher
+        .chars()
+        .filter(|x| x.is_ascii_alphanumeric())
+        .map(|x| {
+            if x.is_ascii_alphabetic() {
+                cipher_char(x)
+            } else {
+                x
+            }
+        })
+        .collect::<String>()
+}
+
+fn cipher_char(x: char) -> char {
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    let index = letters.chars().position(|l| l == x).unwrap();
+    letters.chars().rev().nth(index).unwrap()
 }
