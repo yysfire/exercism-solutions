3c9d3fc888bee8a7e64e53b0fdbf76ed721736fc
yysfire <yysfire+repo@gmail.com>
2018-08-09 11:23:45 +0800

    Optimize solution of atbash-cipher

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,44 +1,36 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let plain = plain.to_ascii_lowercase();
-    let iter = plain
-        .chars()
-        .filter(|x| x.is_ascii_alphanumeric())
-        .map(|x| {
-            if x.is_ascii_alphabetic() {
-                cipher_char(x)
-            } else {
-                x
-            }
-        });
-    let mut result = String::new();
-    for (i, c) in iter.enumerate() {
-        if i != 0 && i % 5 == 0 {
-            result.push(' ');
-        }
-        result.push(c);
-    }
+    use std::iter::once;
 
-    result
+    cipher_str(plain)
+        .enumerate()
+        .flat_map(|(i, x)| {
+            if i != 0 && i % 5 == 0 {
+                once(' ').chain(once(x)).take(2)
+            } else {
+                once(x).chain(once(' ')).take(1)
+            }
+        })
+        .collect::<String>()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher
-        .chars()
-        .filter(|x| x.is_ascii_alphanumeric())
-        .map(|x| {
-            if x.is_ascii_alphabetic() {
-                cipher_char(x)
-            } else {
-                x
-            }
-        })
-        .collect::<String>()
+    cipher_str(cipher).collect::<String>()
 }
 
 fn cipher_char(x: char) -> char {
     let letters = "abcdefghijklmnopqrstuvwxyz";
     let index = letters.chars().position(|l| l == x).unwrap();
     letters.chars().rev().nth(index).unwrap()
+}
+
+fn cipher_str<'a>(s: &'a str) -> impl Iterator<Item = char> + 'a {
+    s.chars().filter(|x| x.is_ascii_alphanumeric()).map(|x| {
+        if x.is_ascii_alphabetic() {
+            cipher_char(x.to_ascii_lowercase())
+        } else {
+            x
+        }
+    })
 }
