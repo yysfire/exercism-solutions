7ca7517b1e66a7c5d304a468ed3348a1c94a2101
yysfire <yysfire+repo@gmail.com>
2021-04-13 14:32:51 +0800

    solve space-age

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -2,26 +2,29 @@ package space
 
 type Planet string
 
+// earthYearSeconds seconds of one earth year
+var earthYearSeconds float64 = 31557600
+
 // Age calculate how old someone would be on `planet`
 func Age(seconds float64, planet Planet) float64 {
-	var age float64
+	var ratio float64
 	switch planet {
 	case "Earth":
-		age = seconds / 31557600
+		ratio = 1
 	case "Mercury":
-		age = seconds / (31557600 * 0.2408467)
+		ratio = 0.2408467
 	case "Venus":
-		age = seconds / (31557600 * 0.61519726)
+		ratio = 0.61519726
 	case "Mars":
-		age = seconds / (31557600 * 1.8808158)
+		ratio = 1.8808158
 	case "Jupiter":
-		age = seconds / (31557600 * 11.862615)
+		ratio = 11.862615
 	case "Saturn":
-		age = seconds / (31557600 * 29.447498)
+		ratio = 29.447498
 	case "Uranus":
-		age = seconds / (31557600 * 84.016846)
+		ratio = 84.016846
 	case "Neptune":
-		age = seconds / (31557600 * 164.79132)
+		ratio = 164.79132
 	}
-	return age
+	return seconds / (ratio * earthYearSeconds)
 }
