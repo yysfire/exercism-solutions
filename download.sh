8756b331d43745a8b18767b2bafeb6bbc723e271
yysfire <yysfire+repo@gmail.com>
2021-04-13 13:24:51 +0800

    auto download and submit bash script

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+if [ -z $1 ]; then
+  echo 'Please give an exercise name'
+  exit 1
+fi
+
+exercism download --exercise=$1 --track=go
+git add $1
+git commit -m "download $1"
---
+++ submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+if [ -z $1 ]; then
+  echo 'Please give an exercise name'
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  msg="solve $1"
+else
+  msg=$2
+fi
+
+git add $1/
+git diff --name-only --cached | tr '\n' ' ' | xargs exercism submit
+git commit -m "${msg}"
