6dedd814a59032c970576be58ceba21845725b76
yysfire <yysfire+repo@gmail.com>
2021-04-14 16:24:05 +0800

    solve hamming in go track

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,27 @@
 package hamming
 
+import (
+	"errors"
+	"strings"
+)
+
+//Distance calculate the Hamming distance between two DNA strands.
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("length of a not equal to b")
+	}
+
+	distance := 0
+	bReader := strings.NewReader(b)
+	for _, ach := range a {
+		bch, _, err := bReader.ReadRune()
+		if err != nil {
+			return 0, err
+		}
+		if ach != bch {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
