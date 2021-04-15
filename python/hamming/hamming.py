606742f8c8afb65f4b0c34ea26333eb7d8c5416e
yysfire <yysfire+repo@gmail.com>
2021-04-15 21:19:24 +0800

    solve hamming in python track

---
+++ python/hamming/hamming.py
@@ -0,0 +1,10 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError('The Hamming distance is only defined for sequences of equal length')
+
+    count = 0
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
+            count += 1
+
+    return count
