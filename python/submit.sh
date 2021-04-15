f3244fd29f09d6918b4f4faf99b354e9a7e2a8c0
yysfire <yysfire+repo@gmail.com>
2021-04-15 21:19:10 +0800

    Begin python track

---
+++ python/submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+if [ -z $1 ]; then
+  echo 'Please give an exercise name'
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  msg="solve $1 in python track"
+else
+  msg=$2
+fi
+
+git add $1/
+#git diff --name-only --cached | tr '\n' ' ' | xargs exercism submit
+git diff --name-status --cached | grep -v D | sed 's|[^/]\+/||' | tr '\n' ' ' | xargs exercism submit
+git commit -m "${msg}"
