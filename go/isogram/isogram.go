57fd89263f6578cda08f4e6df4eb365adbd357ab
yysfire <yysfire+repo@gmail.com>
2021-05-04 14:39:29 +0800

    solve isogram in go track

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,22 @@
+//Package isogram provides a function to determine if a word or phrase is an isogram.
+//An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter,
+//however spaces and hyphens are allowed to appear multiple times.
 package isogram
+
+import "unicode"
+
+//IsIsogram determine if a word or phrase is an isogram.
+func IsIsogram(phrase string) bool {
+	repeatMap := make(map[rune]bool)
+	for _, r := range phrase {
+		if r == '-' || unicode.IsSpace(r) {
+			continue
+		}
+		r = unicode.ToLower(r)
+		if _, ok := repeatMap[r]; ok {
+			return false
+		}
+		repeatMap[r] = true
+	}
+	return true
+}
