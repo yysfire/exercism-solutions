e34182c216e799f420c7a6157bf2665dab8c5b45
yysfire <yysfire+repo@gmail.com>
2021-05-08 22:03:11 +0800

    use the formulas

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -4,20 +4,14 @@ package diffsquares
 
 //SquareOfSum return the square of the sum of the first n natural numbers.
 func SquareOfSum(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
+	sum := (n + 1) * n / 2
 	return sum * sum
 }
 
 //SumOfSquares return the sum of the squares of the first n natural numbers.
+//https://en.wikipedia.org/wiki/Summation#Powers_and_logarithm_of_arithmetic_progressions
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-	return sum
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 //Difference return the difference between the square of the sum and
