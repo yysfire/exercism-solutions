c22b2bb261c855075cbe5d8e6929a5b1f62df253
yysfire <yysfire+repo@gmail.com>
2021-05-09 17:49:31 +0800

    update anagram solution

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -4,15 +4,17 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut result: HashSet<&str> = HashSet::new();
 
-    let mut word_sorted: Vec<char> = word.to_lowercase().chars().collect();
+    let word_string = word.to_lowercase();
+    let mut word_sorted: Vec<char> = word_string.chars().collect();
     word_sorted.sort_unstable();
 
     for anagram in possible_anagrams {
-        if anagram.to_lowercase() == word.to_lowercase() {
+        let anagram_string = anagram.to_lowercase();
+        if anagram_string == word_string {
             continue;
         }
 
-        let mut anagram_sorted: Vec<char> = anagram.to_lowercase().chars().collect();
+        let mut anagram_sorted: Vec<char> = anagram_string.chars().collect();
         anagram_sorted.sort_unstable();
 
         if word_sorted == anagram_sorted {
