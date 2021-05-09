77e84f799cc0f086532f8bfb286848c80e1105b4
yysfire <yysfire+repo@gmail.com>
2021-05-09 16:22:04 +0800

    solve matching-brackets in rust track

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,27 @@
+///Check if the string contains balanced brackets.
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut brackets = vec![];
+
+    string
+        .chars()
+        .filter(|&c| "()[]{}".contains(c))
+        .map(|c| {
+            if "([{".contains(c) {
+                match c {
+                    '(' => brackets.push(')'),
+                    '[' => brackets.push(']'),
+                    '{' => brackets.push('}'),
+                    _ => (),
+                };
+                true
+            } else {
+                if let Some(b) = brackets.pop() {
+                    b == c
+                } else {
+                    false
+                }
+            }
+        })
+        .all(|x| x)
+        && brackets.len() == 0
 }
