18fc974e7dc63b5ab426e445abd98f4ebca11574
yysfire <yysfire+repo@gmail.com>
2021-05-09 13:49:14 +0800

    update solution

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,43 +1,24 @@
 pub fn reply(message: &str) -> &str {
-    match judge(message) {
-        Message::Question => "Sure.",
-        Message::Yell => "Whoa, chill out!",
-        Message::YellQuestion => "Calm down, I know what I'm doing!",
-        Message::Nothing => "Fine. Be that way!",
-        Message::Anything => "Whatever.",
-    }
-}
-
-
-enum Message {
-    Question,
-    Yell,
-    YellQuestion,
-    Nothing,
-    Anything,
-}
-
-
-fn judge(message: &str) -> Message {
     let message = message.trim();
     if message.is_empty() {
-        return Message::Nothing;
+        return "Fine. Be that way!";
     }
-    let is_upper = is_ascii_uppercase(message);
-    if is_upper && message.ends_with('?') {
-        return Message::YellQuestion;
-    } else if is_upper {
-        return Message::Yell;
+
+    let is_all_letters_upper = is_all_letters_uppercase(message);
+
+    if is_all_letters_upper && message.ends_with('?') {
+        "Calm down, I know what I'm doing!"
+    } else if is_all_letters_upper {
+        "Whoa, chill out!"
     } else if message.ends_with('?') {
-        return Message::Question;
+        "Sure."
     } else {
-        return Message::Anything;
+        "Whatever."
     }
 }
 
-
-// Checks if all characters in string are ASCII uppercase character
-fn is_ascii_uppercase(msg: &str) -> bool {
+// Checks if all letters in string are ASCII uppercase
+fn is_all_letters_uppercase(msg: &str) -> bool {
     let mut flag = false;
     for c in msg.chars() {
         if c.is_ascii_uppercase() {
