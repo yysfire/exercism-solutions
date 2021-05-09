f643d7b923b0b755075e3c3b1fa9eca2dce05dde
yysfire <yysfire+repo@gmail.com>
2021-05-09 20:45:45 +0800

    solve sum-of-multiples in go track

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,27 @@
+// Package summultiples provides a function to calculate the sum of
+// all the unique multiples of particular numbers up to but not including the given number.
 package summultiples
+
+// SumMultiples return the sum of all the unique multiples of
+// particular numbers up to but not including the given number.
+func SumMultiples(limit int, divisors ...int) int {
+	multiples := make(map[int]bool)
+	for _, dv := range divisors {
+		if dv == 0 {
+			continue
+		}
+
+		for i := 1; i <= limit/dv; i++ {
+			m := i * dv
+			if m < limit {
+				multiples[m] = true
+			}
+		}
+	}
+
+	sum := 0
+	for m := range multiples {
+		sum += m
+	}
+	return sum
+}
