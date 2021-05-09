f39003ee401cbf9ed8057a034afea9aaf06d3da9
yysfire <yysfire+repo@gmail.com>
2021-05-09 14:58:43 +0800

    update solution: use iterator instead of for loop

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -2,15 +2,9 @@ use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
     let mut set = HashSet::new();
-    for c in candidate.to_lowercase().chars() {
-        if c.is_whitespace() || c == '-' {
-            continue;
-        }
-        if !c.is_alphabetic() || set.contains(&c) {
-            return false;
-        }
-        set.insert(c);
-    }
-
-    true
+    candidate
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .all(|c| set.insert(c))
 }
